ec9ddb61f83c415d46ca4f28026d6591d65bbd7b
nhannafi <nour.hannafi@engie.com>
2020-07-24 15:27:08 +0200

    add part3+4

--- model_sqlite.py
+++ model_sqlite.py
@@ -1,116 +1,153 @@
-#!/usr/bin/env python3
-import os
-import socket
 import sqlite3
-from itertools import chain
-from random import choice
-from string import ascii_letters, digits
-from flask import request
-from datetime import datetime
 
+# ---------------------------------------------------------
 
+def createTables():
+    conn = sqlite3.connect('shareCode.db')
+    c = conn.cursor()
 
+    c.execute("DROP TABLE code")
+    c.execute("DROP TABLE edition")
 
-def create_uid_sqlite(n=9):
-   '''Génère une chaîne de caractères alétoires de longueur n
-   en évitant 0, O, I, l pour être sympa.'''
-   chrs = [ c for c in chain(ascii_letters,digits)
-                        if c not in '0OIl'  ]
-   return ''.join( ( choice(chrs) for i in range(n) ) )
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS code (
+            uid INTEGER PRIMARY KEY AUTOINCREMENT,
+            code TEXT DEFAULT 'Insert your code here ...',
+            language VARCHAR(50) DEFAULT 'py',
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updatedAt TIMESTAMP
+        )
+    ''')
 
-def save_doc_as_file_sqlite(uid=None, code=None, langage=None):
-    '''Crée/Enregistre le document sous la forme d'un fichier
-    data/uid. Return the file name.
-    '''
-    now = datetime.now()  # current date and time
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    null = None
-    cmd = request.user_agent.browser
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS edition (
+            uid INTEGER PRIMARY KEY AUTOINCREMENT,
+            code_id INTEGER,
+            ip VARCHAR(20),
+            user_agent VARCHAR(1000),
+            date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
 
-    if uid is None:
-        uid = create_uid_sqlite()
-        code = '# Write your code here...'
-        langage = ''
+    conn.commit()
+    conn.close()
 
-        connection = sqlite3.connect('tp.db')
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO SHARECODE VALUES(?, ?, ?)", (uid, code, langage))
-        cursor.execute("INSERT INTO USER VALUES(?, ?, ?, ?, ?)", (null, uid, IPAddr, cmd, date_time))
-        connection.commit()
-        connection.close()
-    connection = sqlite3.connect('tp.db')
-    cursor1 = connection.cursor()
-    cursor1.execute('''UPDATE SHARECODE SET code = ?, langage = ? WHERE uid = ? ''', (code, langage, uid))
-    cursor1.execute(
-        '''UPDATE USER SET timestamp = ?, Ip = ?, navigator = ?  WHERE uid = ? ''',
-        (date_time, IPAddr, cmd, uid))
-    connection.commit()
-    connection.close()
-    with open('data/{}'.format(uid),'w') as fd:
-        fd.write(code)
-    with open('data/{}'.format(uid + '.lang'), 'w') as fd:
-        fd.write(langage)
+# ---------------------------------------------------------
+
+def createCode():
+    conn = sqlite3.connect('shareCode.db')
+    c = conn.cursor()
+
+    c.execute('''
+        INSERT INTO code DEFAULT VALUES
+    ''')
+
+    uid = c.lastrowid
+
+    conn.commit()
+    conn.close()
+
     return uid
 
-def read_doc_as_file_sqlite(uid):
-    '''Lit le document data/uid'''
-    try:
-        with open('data/{}'.format(uid)) as fd:
-            code = fd.read()
-        return code
-    except FileNotFoundError:
-        return None
+# ---------------------------------------------------------
 
-def get_last_entries_from_files_sqlite(n=10, nlines=10):
-    entries = os.scandir('data')
-    d = []
-    entries = sorted(list(entries),
-                     key=(lambda e: e.stat().st_mtime),
-                     reverse=True)
-    for i,e in enumerate(entries):
-        if i >= n:
-            break
-        if e.name.startswith('.'):
-            continue
-        with open('data/{}'.format(e.name)) as fd:
-            code = ''.join(( fd.readline() for i in range(nlines) ))
-            if fd.readline():
-                code += '\n...'
-        d.append({ 'uid':e.name, 'code':code })
-    return d
+def getCode(uid):
+    conn = sqlite3.connect('shareCode.db')
+    print(uid)
+    c = conn.cursor()
+    
+    c.execute('''
+        SELECT
+            code,
+            language
+        FROM code
+        WHERE uid = ?
+    ''', uid)
 
-def get_last_entries_from_files_admin_sqlite(n=10, nlines=10):
-    entries = os.scandir('data')
-    d = []
-    user = ""
-    entries = sorted(list(entries),
-                     key=(lambda e: e.stat().st_mtime),
-                     reverse=True)
-    for i, e in enumerate(entries):
-        if i >= n:
-            break
-        if '.lang' in e.name:
-            continue
-        with open('data/{}'.format(e.name)) as fd:
-            code = ''.join((fd.readline() for i in range(nlines)))
-            if fd.readline():
-                code += '\n...'
-        uid = str(e.name)
-        print(uid)
-        connection = sqlite3.connect('tp.db')
-        cursor1 = connection.cursor()
-        ip = cursor1.execute('''SELECT Ip FROM USER where uid = ? ''', (uid,));
-        ip = ip.fetchone()
-        navigator = cursor1.execute('''SELECT navigator FROM USER where uid = ? ''', (uid,));
-        navigator = navigator.fetchone()
-        timestamp = cursor1.execute('''SELECT timestamp FROM USER where uid = ? ''', (uid,));
-        timestamp = timestamp.fetchone()
-        print(user)
-        connection.commit()
-        connection.close()
-        d.append({'uid': e.name, 'code': code, 'ip': ip, 'navigator': navigator, 'timestamp': timestamp,})
-    print(user)
-    return d
+    result = c.fetchone()
 
+    conn.commit()
+    conn.close()
+
+    return result
+
+# ---------------------------------------------------------
+
+def getAllCode():
+    conn = sqlite3.connect('shareCode.db')
+    c = conn.cursor()
+
+    c.execute('''
+        SELECT
+            uid,
+            code,
+            language
+        FROM code
+        ORDER BY
+            updatedAt DESC,
+            createdAt DESC
+    ''')
+
+    result = c.fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+# ---------------------------------------------------------
+
+def updateCode(uid, code, language):
+    conn = sqlite3.connect('shareCode.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+        UPDATE code
+        SET
+            code= ?,
+            language = ?,
+            updatedAt = CURRENT_TIMESTAMP
+        WHERE uid = ?
+    ''', (code, language, uid))
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+# ---------------------------------------------------------
+
+def createEdition(code_id, ip, user_agent):
+    conn = sqlite3.connect('shareCode.db')
+    c = conn.cursor()
+
+    c.execute('''
+        INSERT INTO edition(code_id, ip, user_agent)
+        VALUES(?, ?, ?)
+    ''', (code_id, ip, user_agent))
+
+    uid = c.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return uid
+
+# ---------------------------------------------------------
+
+def getEdition():
+    conn = sqlite3.connect('shareCode.db')
+    c = conn.cursor()
+
+    c.execute('''
+        SELECT code_id, ip, user_agent, date
+        FROM edition
+        ORDER BY date DESC
+    ''')
+
+    result = c.fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
--- sharecodedb.py
+++ sharecodedb.py
@@ -1,74 +1,105 @@
-import sqlite3
-
-from flask import Flask, render_template, \
+from flask import Flask, request, render_template, \
     redirect
-from flask import request
-from model_sqlite import save_doc_as_file_sqlite, \
-    read_doc_as_file_sqlite, \
-    get_last_entries_from_files_sqlite, \
-    get_last_entries_from_files_admin_sqlite
 
-connection = sqlite3.connect('tp.db')
+from model_sqlite import createTables, \
+    createCode, \
+    getCode, \
+    getAllCode, \
+    updateCode, \
+    createEdition, \
+    getEdition
 
-cursor = connection.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS SHARECODE(uid CHAR(50) PRIMARY KEY, code text, langage CHAR(50))')
-cursor.execute('CREATE TABLE IF NOT EXISTS USER(id INTEGER PRIMARY KEY, uid CHAR(50), Ip CHAR(50), navigator CHAR(50), timestamp CHAR(50))')
-connection.commit()
-connection.close()
-
-
-#cursor.execute("INSERT INTO SHARECODE VALUES('1', 'test', 'test')")
+from colorizing import colorize
 
 app = Flask(__name__)
+createTables()
+
+# ---------------------------------------------------------
 
 @app.route('/')
 def index():
-    #d = { 'last_added':[ { 'uid':'testuid', 'code':'testcode' } ] }
-    d = { 'last_added':get_last_entries_from_files_sqlite() }
-    return render_template('index.html',**d)
+    data = getAllCode()
+    html = []
 
-@app.route('/create')
+    for row in data :
+        d = dict(
+            uid = row[0],
+            code = colorize(row[1], row[2]),
+            language = row[2]
+        )
+
+        html.append(d)
+
+    return render_template('index.html', data = html)
+
+# ---------------------------------------------------------
+
+@app.route('/create/')
 def create():
-    uid = save_doc_as_file_sqlite()
+    uid = createCode()
+    createEdition(uid, request.remote_addr, request.user_agent.string)
     return redirect("{}edit/{}".format(request.host_url,uid))
+
+# ---------------------------------------------------------
 
 @app.route('/edit/<string:uid>/')
 def edit(uid):
-    code = read_doc_as_file_sqlite(uid)
-    if code is None:
-        return render_template('error.html',uid=uid)
-    d = dict( uid=uid, code=code,
-              url="{}view/{}".format(request.host_url,uid))
-    return render_template('edit.html', **d)
+    row = getCode(uid)
+
+    if row is None:
+        return render_template('error.html',uid = uid)
+
+    d = dict(
+        uid=uid,
+        code=row[0],
+        language=row[1],
+        url="{}view/{}".format(request.host_url,uid)
+    )
+
+    return render_template('edit.html', **d) 
+
+# ---------------------------------------------------------
 
 @app.route('/publish',methods=['POST'])
 def publish():
     code = request.form['code']
     uid  = request.form['uid']
-    langage = request.form['langage']
+    language  = request.form['language']
 
-    save_doc_as_file_sqlite(uid,code,langage)
+    updateCode(uid, code, language)
+    createEdition(uid, request.remote_addr, request.user_agent.string)
 
-    return redirect("{}{}/{}".format(request.host_url,
-                                     request.form['submit'],
-                                     uid))
+    return redirect(
+        "{}{}/{}".format(request.host_url,
+        request.form['submit'],
+        uid)
+    )
+
+# ---------------------------------------------------------
 
 @app.route('/view/<string:uid>/')
 def view(uid):
-    code = read_doc_as_file_sqlite(uid)
-    if code is None:
-        return render_template('error.html',uid=uid)
-    d = dict( uid=uid, code=code,
-              url="{}view/{}".format(request.host_url,uid))
+    row = getCode(uid)
+
+    if row is None:
+        return render_template('error.html',uid = uid)
+
+    d = dict(
+        uid=uid,
+        code = colorize(row[0], row[1]),
+        language=row[1],
+        url="{}view/{}".format(request.host_url,uid)
+    )
+
     return render_template('view.html', **d)
+
+# ---------------------------------------------------------
 
 @app.route('/admin/')
 def admin():
-    pass
-    d = {'last_added':get_last_entries_from_files_admin_sqlite()}
-    code = 'print "Hello World"'
-    print(highlight(code, PythonLexer(), HtmlFormatter()))
-    return render_template('admin.html', **d)
+    return render_template('admin.html', data = getEdition())
+
+# ---------------------------------------------------------
 
 if __name__ == '__main__':
     app.run()
